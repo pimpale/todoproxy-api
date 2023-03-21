eace6c28824f631d3531bc85858bf98dbe7ff13a
gpi <gpimpale29@gmail.com>
2023-03-21 16:39:48 -0700

    add seperate remove method

--- src/request.rs
+++ src/request.rs
@@ -10,7 +10,12 @@ pub struct WebsocketInitMessage {
 pub struct HabiticaIntegrationNewProps {
     pub integration_user_id: String,
     pub integration_api_key: String,
-    pub active: bool,
+    pub api_key: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct HabiticaIntegrationDeleteProps {
     pub api_key: String,
 }
 
